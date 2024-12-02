10e247cab0cd15e34ec073c43c6661e06b7b2928
talg2008 <talginzburg2008@gmail.com>
2024-12-02 16:21:58 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,48 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+package talg;
+
+public class eww {
+	
+	public static Node<Integer> exe1random(int n,int x,int y)
+	{
+		Node <Integer> first=new Node <>((int)(Math.random()*(y-x+1))+x);
+		Node <Integer> current=first;
+		Node <Integer> next;
+		for(int i=1; i<n; i++)
+		{
+			next=new Node <>((int)(Math.random()*(y-x+1))+x);
+			current.setNext(next);
+			current=next;
+			
+			
+
+		}
+		return first;
+	}
+	
+	public static int exe2find(int x, Node <Integer> h)
+	{
+		int count=0;
+		Node <Integer> current =h;
+		while(current!=null)
+		{
+			if(current.getValue()==x)
+			{
+				count++;
+			}
+			current=current.getNext();
+		}
+		return count;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		Node <Integer> x=exe1random(5,1,10);
+		System.out.println(x);
+		System.out.println(exe2find(4,x));
+		
+
+	}
+
 }
+
